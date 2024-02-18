c88c399f22dacbf433931e9349a7e4f48d924d99
arun <arunmahara124@gmail.com>
2024-02-18 19:31:00 +0545

    feat: get arguments from command

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import argparse
 from pdf_to_csv import extract_table_from_pdf
 
 
-def main():
-    extract_table_from_pdf("example_pdfs/pdf5.pdf", ".")
+def main(pdf_file: str, output_folder: str):
+    extract_table_from_pdf(pdf_file, output_folder)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Extract tables from PDF file.")
+    parser.add_argument('-s', '--source', help="Path to the PDF file", dest="pdf_file")
+    parser.add_argument('-o', '--output', help="Path to the output folder", dest="output_folder")
+    args = parser.parse_args()
+
+    assert args.pdf_file is not None, "Must provide a PDF file!"
+    assert args.output_folder is not None, "Must provide a output folder!"
+
+    main(args.pdf_file, args.output_folder)
